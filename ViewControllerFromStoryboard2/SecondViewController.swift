01f3b4516ddd6f35272ba82253cc54b980ec9393
DanRadchukDev <dan.radchuk.dev@gmail.com>
2023-01-25 01:15:02 -0600

    second try

---
+++ ViewControllerFromStoryboard2/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  ViewControllerFromStoryboard2
+//
+//  Created by Dan Radchuk on 1/25/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
+    
+
+}
--- ViewControllerFromStoryboard2/ViewController.swift
+++ ViewControllerFromStoryboard2/ViewController.swift
@@ -11,9 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .lightGray
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapButton() {
+        guard let vc = storyboard?.instantiateViewController(identifier: "second") as? SecondViewController else {
+            print ("failed to get vc from storyboard")
+            return
+        }
+            
+        present(vc, animated: true)
+        
+    }
+    
 }
 
